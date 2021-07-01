94c169db404c3f1b67347fccfcddf7fbd976aa2b
Your Name <you@example.com>
2021-07-01 00:31:20 -0300

    first commit

---
+++ src/mocks/heroesMock.ts
@@ -0,0 +1,13 @@
+const heroesMock = [
+    { id: 12, name: 'Batman' },
+    { id: 13, name: 'Superman' },
+    { id: 14, name: 'Wolverine' },
+    { id: 15, name: 'Papa Pitufo' },
+    { id: 16, name: 'Heman' },
+    { id: 17, name: 'Robotech' },
+    { id: 18, name: 'Mazinger Z' },
+    { id: 19, name: 'Patan' },
+    { id: 20, name: 'Diego Maradona' },
+  ];
+  
+  export default heroesMock;
